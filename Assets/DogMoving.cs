35d9ded2bfdba643dde17553ed198ab22afcc6e6
Wise <h34091209@gs.ncku.edu.tw>
2023-10-05 00:55:39 +0800

    1004狗會轉向，會叫看到不叫，有顯示資訊

--- Assets/DogMoving.cs
+++ Assets/DogMoving.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DogMoving : MonoBehaviour
@@ -10,12 +11,25 @@ public class DogMoving : MonoBehaviour
     private float lastWaypointSwitchTime;
     public float speed = 1.0f;
 
-    private bool isCollisionOnTriggered = false;
     private bool lookAt = false;
+
+    public Animator animator;
+    float control = 0.0f;
+    public float acceleration = 0.5f;
+    int VelocityHash;
+
+    public AudioSource audioSource;
+    public TextMeshProUGUI Information;
     // Start is called before the first frame update
     void Start()
     {
         lastWaypointSwitchTime = Time.time;
+        gameObject.transform.position = waypoints[0].transform.position;
+        animator = GetComponent<Animator>();
+        //VelocityHash = Animator.StringToHash("control");
+        audioSource = gameObject.GetComponent<AudioSource>();
+        //AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+        Information.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,16 +39,30 @@ public class DogMoving : MonoBehaviour
         {
             Debug.Log("不要動");
             Time.timeScale = 0;
+            //audioSource = GetComponent<AudioSource>();
+            audioSource.Pause();
+            Information.gameObject.SetActive(true);
         }
         else
         {
             Time.timeScale = 1.0f;
+            animator.SetBool("turn", false);
             MovingOnRoad();
+            audioSource.UnPause();
+            //audioSource.loop = true;
+            //audioSource.Play();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            Information.gameObject.SetActive(false);
+
         }
     }
 
     void MovingOnRoad()
     {
+        
         //設定移動路徑的起終點
         Vector3 startPosition = waypoints[currentWaypoint].transform.position;
         Vector3 endPosition = waypoints[currentWaypoint + 1].transform.position;
@@ -45,7 +73,14 @@ public class DogMoving : MonoBehaviour
         float totalTimeForPath = pathLength / speed;
         float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
         gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
-        //
+        //調整blend的數值
+        if(control<=1)
+        {
+            control = (currentTimeOnPath / totalTimeForPath)*acceleration;
+        }
+        animator.SetFloat("control", control);
+        Debug.Log("blend值" + control);
+
         Debug.Log("預計總花費時間" + totalTimeForPath);
         Debug.Log("在這條路上跑多久了" + currentTimeOnPath);
         Debug.Log("狗的位子" + gameObject.transform.position);
@@ -60,8 +95,10 @@ public class DogMoving : MonoBehaviour
                 Debug.Log("成功++");
                 RotateIntoMoveDirection();
                 //換成走向下一個點
-                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-                //AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+                //做動畫
+                animator.SetBool("turn", true);
+                animator.SetFloat("control", 1);
+
                 audioSource.Play();
             }
             else
@@ -78,20 +115,17 @@ public class DogMoving : MonoBehaviour
         Vector3 newStartPosition = waypoints[currentWaypoint].transform.position;
         Vector3 newEndPosition = waypoints[currentWaypoint + 1].transform.position;
         Vector3 newDirection = (newEndPosition - newStartPosition);
+        Debug.Log("新方向" + newDirection);
         //利用Mathf.Atan2來決定要轉多少度
         float x = newDirection.x;
         float y = newDirection.y;
         float z = newDirection.z;
-        float rotationAngle = Mathf.Atan2(z, x) * 180 / Mathf.PI;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, newDirection, 0.1f, 0.0f);
-        //轉動dog的子物件
-        GameObject default1 = gameObject.transform.Find("default").gameObject;
-        //default1.transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
-        //default1.transform.RotateAround(default1.transform.position, Vector3.up, rotationAngle);
-        //default1.transform.rotation = Quaternion.LookRotation(newDir);
-        default1.transform.rotation = Quaternion.identity; // 重置旋转
-        default1.transform.Rotate(Vector3.up, 360 - rotationAngle); // 应用新的旋转
-
+        float rotationAngle = Mathf.Atan2(x, z) * 180 / Mathf.PI;
+        Debug.Log("轉幾度" + rotationAngle);
+        //轉動dog
+        //gameObject.transform.localEulerAngles = new Vector3(0.0f, rotationAngle, 0.0f);
+        gameObject.transform.rotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
+        Debug.Log("轉了"+gameObject.transform.rotation);
     }
 
     public void test()
---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mainCameraTransform.position;
+        transform.rotation = mainCameraTransform.rotation;
+    }
+}
