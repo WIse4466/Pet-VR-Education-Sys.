8c31a08dc13665cf71067fd1adc211e62b2cccac
Wise <h34091209@gs.ncku.edu.tw>
2023-10-14 13:51:27 +0800

    1012 加第二個場景，柯基免疫系統失衡

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -4,8 +4,55 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
-    public void test()
+    public Animator animator;
+    public AudioSource audioSource;
+    int watchTimes = 0;
+    public float speed = 1.0f;
+    public Vector3 target;
+    float elaspedTime = 0.0f;
+    float duration = 2.0f;
+    float t;
+
+    private void Start()
     {
-        Debug.Log("test");
+        animator = GetComponent<Animator>();
+        //animator.SetBool("watching", false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        elaspedTime += Time.deltaTime;
+        t = Mathf.Clamp01(elaspedTime / duration);
+    }
+
+    public void watch()
+    {
+        animator.SetBool("watching", true);
+        audioSource.Play();
+        animator.SetInteger("watchTimes", watchTimes);
+        if(watchTimes >0)
+        {
+            //moving();
+        }
+        watchTimes++;
+        Debug.Log("watching"+watchTimes);
+    }
+
+    public void lookAround()
+    {
+        animator.SetBool("watching", false);
+        audioSource.Pause();
+        Debug.Log("看周圍");
+    }
+
+    public void moving()
+    {
+        //transform.Translate(Vector3.back * speed, Space.World);
+        float moveDistance = 0.1f;
+        target = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveDistance);
+        //transform.position = Vector3.MoveTowards(transform.position, target, speed);
+        transform.position = Vector3.Lerp(transform.position, target, t);
+        
     }
 }
