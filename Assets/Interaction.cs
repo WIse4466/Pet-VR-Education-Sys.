06be1b69fc3595344fc81b658690be4a1c15d713
Wise <h34091209@gs.ncku.edu.tw>
2023-10-17 13:57:48 +0800

    第二個柯基的場景，免疫系統失衡，只差移動、排成

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -8,11 +8,12 @@ public class Interaction : MonoBehaviour
     public Animator animator;
     public AudioSource audioSource;
     int watchTimes = 0;
-    public float speed = 1.0f;
+    public float speed = 0.0001f;
     public Vector3 target;
-    float elaspedTime = 0.0f;
-    float duration = 2.0f;
-    float t;
+    int answer = 0;
+    bool move = false;
+    float startMovingTime;
+    Vector3 endPosition;
 
     private void Start()
     {
@@ -23,15 +24,28 @@ public class Interaction : MonoBehaviour
 
     private void Update()
     {
-        elaspedTime += Time.deltaTime;
-        t = Mathf.Clamp01(elaspedTime / duration);
+        //elaspedTime += Time.deltaTime;
+        //t = Mathf.Clamp01(elaspedTime / duration);
+        if(Input.GetButtonDown("Fire3"))
+        {
+            animator.SetBool("feeding mode", true);
+            animator.SetInteger("correct", answer);
+            move = true;
+            startMovingTime = Time.time;
+            endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+
+        if(move == true)
+        {
+            moving();
+        }
     }
 
     public void watch()
     {
         animator.SetBool("watching", true);
         audioSource.Play();
-        animator.SetInteger("watchTimes", watchTimes);
+        animator.SetInteger("watchTimes", watchTimes % 2);
         if(watchTimes >0)
         {
             //moving();
@@ -50,10 +64,26 @@ public class Interaction : MonoBehaviour
     public void moving()
     {
         //transform.Translate(Vector3.back * speed, Space.World);
-        float moveDistance = 0.1f;
-        target = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveDistance);
+        float moveDistance = 1f;
+        //target = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveDistance);
         //transform.position = Vector3.MoveTowards(transform.position, target, speed);
-        transform.position = Vector3.Lerp(transform.position, target, t);
+        //transform.position = Vector3.Lerp(transform.position, target, t);
+
         
+
+        //設定移動路徑的起終點
+        Vector3 startPosition = gameObject.transform.position;
+        //Vector3 endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveDistance);
+        Debug.Log("currentWaypoint起點" + startPosition);
+        //計算兩點間的距離，以此算出需花費時間
+        //currentTimeOnPath可以算出現在離上一個點過多久了，用Lerp可以模擬出他的路徑
+        float pathLength = Vector3.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - startMovingTime;
+        gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+        Debug.Log("預計總花費時間" + totalTimeForPath);
+        Debug.Log("在這條路上跑多久了" + currentTimeOnPath);
+        Debug.Log("狗的位子" + gameObject.transform.position);
+        Debug.Log("終點的位子" + endPosition);
     }
 }
